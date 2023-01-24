bab33a72399a3db53e2a9d27547f694918cb13a1
Marshallcdy <marshallcdy12345@gmail.com>
2023-01-24 09:16:31 +0545

    master

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,9 @@ class MyHomepage extends StatelessWidget {
                   ),
                   child: Column(
                     children: [
+                      const SizedBox(
+                        height: 8,
+                      ),
                       Text(
                         "We will make \n Your Stomach Full\n and Healthy.",
                         style: GoogleFonts.poppins(
@@ -70,13 +73,16 @@ class MyHomepage extends StatelessWidget {
                         textAlign: TextAlign.center,
                       ),
                       const SizedBox(
-                        height: 10,
+                        height: 20,
                       ),
                       CircleAvatar(
-                        radius: 50,
+                        radius: 40,
                         backgroundColor: Colors.white,
                         child: IconButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: ((context) => const Orderpage())));
+                            },
                             icon: const Icon(
                               Icons.arrow_forward_outlined,
                               color: Color(0xffFE8C00),
@@ -89,5 +95,69 @@ class MyHomepage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Orderpage extends StatelessWidget {
+  const Orderpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Stack(children: [
+          Image.asset(
+            "photo/secback.jpg",
+            width: double.infinity,
+          ),
+          const Positioned(
+            top: 50,
+            left: 25,
+            child: Text(
+              "Your Location :",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const Positioned(
+              top: 55,
+              right: 20,
+              child: Icon(
+                Icons.circle_notifications,
+                color: Colors.white,
+                size: 35,
+              )),
+          const Positioned(
+              top: 70,
+              left: 20,
+              child: Icon(
+                Icons.location_on,
+                color: Colors.white,
+                size: 25,
+              )),
+          const Positioned(
+            top: 73,
+            left: 50,
+            child: Text(
+              "Bharatpur,Chitwan ",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const Positioned(
+            top: 115,
+            left: 20,
+            child: Text(
+              "Order your \n Favorite Food",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 32),
+            ),
+          ),
+        ])
+      ],
+    ));
   }
 }
